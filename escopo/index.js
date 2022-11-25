aea7222998ffde27be3b040d708685db4efaa2eb
ciceroduvall23 <74693314+ciceroduvall23@users.noreply.github.com>
2022-11-25 10:06:53 -0300

    update

---
+++ escopo/index.js
@@ -0,0 +1,8 @@
+let x =10;
+
+if(true){
+    let x =20;
+    console.log(x); //escopo if
+}
+
+console.log(x) //escopo global
---
+++ funcoes/index.js
@@ -0,0 +1,11 @@
+function nomeDaPessoa (){
+    console.log("seja bem vindo")
+}
+
+nomeDaPessoa(); /* estamos declarando a função*/
+
+function numeroDaPessoa (num){  /* parametro num*/
+    console.log("O numero é " + num ) /*+ num --> concatenar*/ 
+}
+
+numeroDaPessoa(3); /* estamos declarando a função*/
